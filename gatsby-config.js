7e4c0771597f7e5b1170a2db539ef3176814ea50
RealTiny656 <tiny656@hotmail.com>
2023-03-27 07:57:32 +0800

    Update title and description in gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,10 +1,10 @@
 module.exports = {
   pathPrefix: process.env.PATH_PREFIX || '/',
   siteMetadata: {
-    siteTitle: 'Running Page',
+    siteTitle: 'RealTiny656's Running Page',
     siteUrl: 'https://tiny656.github.io/running_page/',
     logo: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQTtc69JxHNcmN1ETpMUX4dozAgAN6iPjWalQ&usqp=CAU',
-    description: 'Running Page',
+    description: 'RealTiny656's Running Page',
     navLinks: [
       {
         name: 'About',
