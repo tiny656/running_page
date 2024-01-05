b7ce7d0f635b65fb9e3158c9a6749e2df19a2d7c
tiny656 <tiny656@hotmail.com>
2024-01-05 09:58:42 +0800

    update font

--- src/static/site-metadata.ts
+++ src/static/site-metadata.ts
@@ -13,7 +13,7 @@ const data: ISiteMetadataResult = {
   siteTitle: 'RealTiny656 Run',
   siteUrl: 'https://tiny656.github.io/running_page/',
   logo: 'https://ooo.0x0.ooo/2023/12/20/OKzpKP.png',
-  description: 'Personal site',
+  description: 'RealTiny656 Run',
   navLinks: [
     // {
     //   name: 'About',
