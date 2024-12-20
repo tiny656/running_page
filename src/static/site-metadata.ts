954b2f1b8ccebb2ed72dd683e480d129c6155161
RealTiny656 <tiny656@hotmail.com>
2024-12-20 14:20:39 +0800

    Update logon in site-metadata.ts

--- src/static/site-metadata.ts
+++ src/static/site-metadata.ts
@@ -12,7 +12,7 @@ interface ISiteMetadataResult {
 const data: ISiteMetadataResult = {
   siteTitle: 'RealTiny656 Run',
   siteUrl: 'https://tiny656.github.io/running_page/',
-  logo: 'https://ooo.0x0.ooo/2023/12/20/OKzpKP.png',
+  logo: 'https://ooo.0x0.ooo/2024/12/20/OE5H71.png',
   description: 'RealTiny656 Run',
   navLinks: [
     // {
