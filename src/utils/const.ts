1131ad790d585f0093a4b6fdf8b66b087247d54f
RealTiny656 <tiny656@hotmail.com>
2025-09-02 20:04:08 +0800

    Update const.ts

--- src/utils/const.ts
+++ src/utils/const.ts
@@ -3,7 +3,7 @@ const MAPBOX_TOKEN =
   // For security reasons, please avoid using the default public token provided by Mapbox as much as possible.
   // Instead, manually add a new token and apply URL restrictions.
   // (please refer to https://github.com/yihong0618/running_page/issues/643#issuecomment-2042668580)
-  'pk.eyJ1IjoidGlueTY1NiIsImEiOiJjbHcydG9lbzAwcWFkMmtxcXRhd3N6ZzR0In0._QeFjOvVqrj_g0bGVaIpdA';
+  'pk.eyJ1IjoidGlueTY1NiIsImEiOiJjbWYyaTJkcDcwNmZ0MnFzNW5naHZsc25oIn0.DKnvTe_n7w_jGVyAP_riNA';
 const MUNICIPALITY_CITIES_ARR = [
   '北京市',
   '上海市',
